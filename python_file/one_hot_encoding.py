083cad15ae8f339559a7674babd5766da982ba89
전성일 <jeonjerry0709@gmail.com>
2024-10-17 18:46:59 +0900

    feat : add One-Hot Encoding example

--- python_file/one_hot_encoding.py
+++ python_file/one_hot_encoding.py
@@ -1,13 +1,16 @@
-from tensorflow.keras.preprocessing.text import Tokenizer 
+from tensorflow.keras.preprocessing.text import Tokenizer
+from tensorflow.keras.utils import to_categorical
 
 text = "안녕 클레오파트라 세상에서 제일 가는 포테이토칩 안녕 안녕 클레오파트라 세상에서 제일 가는" 
 
 token = Tokenizer()
-token.fit_on_texts([text])# 입력한 텍스트를 단어 빈도수가 높은 순에서 낮은 순으로 순차적으로 정수 인덱스를 부여
-print(token.word_index)
+token.fit_on_texts([text])
+print(f"{token.word_index}\n")#토큰화된 단어와 해당하는 인덱스
 
-sub_text = "세상에서 제일 가는 포테이토칩 안녕"#인코딩을 위한 텍스트
+x = token.texts_to_sequences([text])#토큰의 인덱스로만 이루어진 리스트
+print(text)#원본 텍스트 출력
+print(f"{x}\n")#토큰의 인덱스로만 이루어진 리스트 출력
 
-
-encoded = token.texts_to_sequences([sub_text])[0]#입력한 텍스트를 정수 인덱스로 변환
-print(encoded)
+word_size = len(token.word_index)+1
+x = to_categorical(x, num_classes=word_size)#원핫인코딩
+print(f"{x}\n")#원핫인코딩된 리스트 출력
